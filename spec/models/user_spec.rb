cfed0e026eece12ddef4b14991b35ae9ec716045
okochang <hideaki.yanase@gmail.com>
2015-04-22 01:47:02 +0900

    6章の演習を終えた

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,7 +34,7 @@ describe User do
 
   describe "when email format is invalid" do
     it "shoudl be invalid" do
-      address = %w[user@foo,com user_at_foo.org, example.user@foo. foo@bar_baz.com, foo@bar+baz.com]
+      address = %w[user@foo,com user_at_foo.org, example.user@foo. foo@bar_baz.com, foo@bar+baz.com, foo@bar..com]
       address.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -49,6 +49,16 @@ describe User do
         @user.email = valid_address
         expect(@user).to be_valid
       end
+    end
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAmple.Com" }
+
+    it "should be saved as all lower case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
     end
   end
 
