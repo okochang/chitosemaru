e02ea3a2aa185983305bed0dc5046ac33bb134f6
okochang <hideaki.yanase@gmail.com>
2015-04-27 09:53:15 +0900

    FactoryGirlでシュミレートするファクトリーを追加した

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Example User"
+    email "user@exapmle.com"
+    password "examplepassword"
+    password_confirmation "examplepassword"
+  end
+end
